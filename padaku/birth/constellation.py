8e2aefdc8da73e45727c4d388aa26800b49a6297
lirui315 <RuiLi_Violet@163.com>
2018-11-21 17:00:34 -0800

    Violet second submit

--- padaku/birth/constellation.py
+++ padaku/birth/constellation.py
@@ -6,29 +6,29 @@
 
 def constellation(month,date):
     if (month==3 and date>=21) or (month==4 and date<=19):
-        return "Aries"
+        return "Your constellation is Aries"
     elif (month==4 and date>=20) or (month==5 and date<=20):
-        return "Taurus"
+        return "Your constellation is Taurus"
     elif (month==5 and date>=21) or (month==6 and date<=21):
-        return "Gemini"
+        return "Your constellation is Gemini"
     elif (month==6 and date>=22) or (month==7 and date<=22):
-        return "Cancer"    
+        return "Your constellation is Cancer"    
     elif (month==7 and date>=23) or (month==8 and date<=22):
-        return "Leo"
+        return "Your constellation is Leo"
     elif (month==8 and date>=23) or (month==9 and date<=22):
-        return "Virgo"
+        return "Your constellation is Virgo"
     elif (month==9 and date>=23) or (month==10 and date<=23):
-        return "Libra"
+        return "Your constellation is Libra"
     elif (month==10 and date>=24) or (month==11 and date<=22):
-        return "Scorpio" 
+        return "Your constellation is Scorpio" 
     elif (month==11 and date>=23) or (month==12 and date<=21):
-        return "Sagittarius"
+        return "Your constellation is Sagittarius"
     elif (month==12 and date>=22) or (month==1 and date<=19):
-        return "Capricornus"
+        return "Your constellation is Capricornus"
     elif (month==1 and date>=20) or (month==2 and date<=18):
-        return "Aquarius"
+        return "Your constellation is Aquarius"
     else:
-        return "Pisces"
+        return "Your constellation is Pisces"
 
 
 # In[14]:
